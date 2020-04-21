690edb7e7bbdac532d4f842e2c5b282f13b5fa9b
Pete Heyes <peteheyes@Leighs-MBP-4.lan>
2020-04-21 18:13:59 +0100

    wip managing use effect on kills component

--- src/components/Kills.js
+++ src/components/Kills.js
@@ -1,44 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useKills } from '../hooks'
 import { useSelectedCharacterValue } from '../context'
 import { dataCleanUp } from '../helpers'
+import { orderObjectKeys } from '../helpers'
 
 export const Kills = () => {
 
   const { selectedCharacter } = useSelectedCharacterValue()
   const { kills } = useKills(selectedCharacter.characterId)
-  const data = dataCleanUp()
+  const [monsterData, setMonsterData] = useState(dataCleanUp())
+  const [killsData, setKillsData] = useState([])
 
-  // dataCleanUp()
   //this probs needs to be a promise (or just save as variable)
   //so that once the data has been cleaned and kills have been returned we can 
   //do the comparision
-
-  // const findKills = (data, kills) => {
-  //   const totalKills = data.map(monster => {
-  //     console.log(monster.monsterId)
-  //     kills.map(
-  //       console.log(kills.monsterId)
-  //     )
-  //   }
-  //   )
-  //   return totalKills
-  // }
-
-  // useEffect(() => {
-  //   if (!kills.length > 0) {
-  //     return
-  //   }
-  //   findKills()
-
-  // }, [kills])
-
   
-  // const monsterData = findKills(data, kills)
+  const findKills = (monsterData, kills) => {
+    const killsData = []
+    kills.map(kill => {
+      let killedMonster = monsterData.filter(monster => 
+        kill.monsterId == monster.monsterId
+      )
+      killedMonster = Object.assign({}, ...killedMonster, {...kill})
+      killsData.push(killedMonster)
+      return killsData
+    })
+    console.log(killsData)
+    return killsData
+  }
 
-  // console.log(monsterData, 'monsterData')
+  useEffect(() => {
+  if (!kills.length > 0) {
+    return
+  }
+      setKillsData(findKills(monsterData, kills))
+      console.log(killsData)
+  
+  
+}, [monsterData,
+  // kills,
+  killsData])
+
+  // console.log(killsData, 'killsData')
   return (
-    kills.length > 0 && (
+    killsData.length > 0 && (
       <ul>{kills.map(kill =>
         <li key={kill.monsterId}>
           {kill.notes}
