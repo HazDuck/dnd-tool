4a89832271d4b957168e4335deeb623ef3a29fb3
Pete Heyes <peteheyes@Leighs-MBP-4.lan>
2020-05-08 10:37:56 +0100

    mobile header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,21 +9,23 @@ export const Header = () => {
     <header className="header">
       {user && (
         <>
-        <img 
-          onClick={()=> setShowSidebar(!showSidebar)}
-          className="hamburger" 
-          src="/images/interface.png" 
-          alt="hamburger menu"/>
-        <div className="evil-wizard-container">
-          <img className="header-image right" src="/images/evil-wizard.png" alt="evil wizard"/>
-          <img className="speech" src="/images/speech-edit.png" alt="In speech bubble: Greetings traveller...add your character's mighty victories to my library. For knowledge is power! muahahaha!"/>
+        <div className="top-header-container">
+          <img 
+            onClick={()=> setShowSidebar(!showSidebar)}
+            className="hamburger" 
+            src="/images/interface.png" 
+            alt="hamburger menu"/>
+          <div className="evil-wizard-container">
+            <img className="header-image right" src="/images/evil-wizard.png" alt="evil wizard"/>
+            <img className="speech" src="/images/speech-edit.png" alt="In speech bubble: Greetings traveller...add your character's mighty victories to my library. For knowledge is power! muahahaha!"/>
+          </div>
         </div>
         </>
       )}
       <div className="heading-container">
         <h1>Larloch's Library</h1>
         <hr className="golden"></hr>
-        <h3>- Keeping track of who's killed what across the Realms... -</h3>
+        <h3>Keeping track of who's killed what across the Realms</h3>
       </div>
     </header>
   )
