16f50a5169683d99b187e6fde8babc4ca7231f7d
Pete Heyes <peteheyes@Leighs-MBP-4.lan>
2020-05-09 13:06:48 +0100

    deleted loading bar

--- src/components/LoadingBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const LoadingBar = ({loadingValue}) => {
-  return (
-    loadingValue < 100 ? <progress className="nes-progress is-primary" value={loadingValue} max="100"></progress>
-    : <p>This app has been TPKed...sorry</p>
-  )
-}
-
