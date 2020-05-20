9e91d5b990bd5480e412482d9ada41f0f9dfdaad
Pete Heyes <peteheyes@Leighs-MBP-4.lan>
2020-05-20 11:43:07 +0100

    provider delete commented out code

--- src/context/index.js
+++ src/context/index.js
@@ -6,92 +6,12 @@ import {
   useDisplayState, 
   useUser} from '../hooks'
 
-// export const CharacterContext = createContext() 
-// export const CharactersProvider = ({ children }) => {
-//   const { characters, setCharacters } = useCharacters()
-//   return (
-//     <CharacterContext.Provider value={{characters, setCharacters}}>
-//       {children}
-//     </CharacterContext.Provider>
-//   )
-// }
-// export const useCharactersValue = () => useContext(CharacterContext)
-
-// //-----------------------------------------------------------------------//
-
-// export const SelectedCharacterContext = createContext() 
-// export const SelectedCharacterProvider = ({ children }) => {
-//   const { selectedCharacter, setSelectedCharacter } = useSelectedCharacter()
-//   return (
-//     <SelectedCharacterContext.Provider value={{selectedCharacter, setSelectedCharacter}}>
-//       {children}
-//     </SelectedCharacterContext.Provider>
-//   )
-// }
-// export const useSelectedCharacterValue = () => useContext(SelectedCharacterContext)
-
-// // //-----------------------------------------------------------------------//
-
-// export const SelectedMonsterContext = createContext() 
-// export const SelectedMonsterProvider = ({children}) => {
-//   const { selectedMonster, setSelectedMonster } = useSelectedMonster()
-//   return (
-//     <SelectedMonsterContext.Provider value={{selectedMonster, setSelectedMonster}}>
-//       {children}
-//     </SelectedMonsterContext.Provider>
-//   )
-// }
-// export const useSelectedMonsterValue = () => useContext(SelectedMonsterContext)
-
-// //-----------------------------------------------------------------------//
-
-// export const DisplayStateContext = createContext() 
-// export const DisplayStateProvider = ({ children }) => {
-//   const { 
-//     showKillsModal, 
-//     setShowKillsModal, 
-//     showKillsSummary, 
-//     setShowKillsSummary, 
-//     showAddKill, 
-//     setShowAddKill ,
-//     showSidebar, 
-//     setShowSidebar
-//   } = useDisplayState()
-//   return (
-//     <DisplayStateContext.Provider value={{
-//       showKillsModal, 
-//       setShowKillsModal, 
-//       showKillsSummary, 
-//       setShowKillsSummary, 
-//       showAddKill, 
-//       setShowAddKill,
-//       showSidebar, 
-//       setShowSidebar
-//       }}>
-//       {children}
-//     </DisplayStateContext.Provider>
-//   )
-// }
-// export const useDisplayStateContextValue = () => useContext(DisplayStateContext)
-
-// //-----------------------------------------------------------------------//
-
-// export const UserContext = createContext() 
-// export const UserProvider = ({ children }) => {
-//   const { user, setUser } = useUser()
-//   return (
-//     <UserContext.Provider value={{ user, setUser }}>
-//       {children}
-//     </UserContext.Provider>
-//   )
-// }
-// export const useUserValue = () => useContext(UserContext)
-
-//-----------------------------------------------------------------------//
-
 export const AppContext = createContext() 
 export const AppProvider = ({ children }) => {
   const { user, setUser } = useUser()
+  const { selectedCharacter, setSelectedCharacter } = useSelectedCharacter()
+  const { selectedMonster, setSelectedMonster } = useSelectedMonster()
+  const { characters, setCharacters } = useCharacters()
   const { 
     showKillsModal, 
     setShowKillsModal, 
@@ -102,9 +22,6 @@ export const AppProvider = ({ children }) => {
     showSidebar, 
     setShowSidebar
   } = useDisplayState()
-  const { selectedCharacter, setSelectedCharacter } = useSelectedCharacter()
-  const { selectedMonster, setSelectedMonster } = useSelectedMonster()
-  const { characters, setCharacters } = useCharacters()
   return (
     <AppContext.Provider value={{ 
       user, 
